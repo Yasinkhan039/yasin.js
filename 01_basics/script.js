ccf69a1806baf4a36a00f0af64bc5735b4a59688
YasinKhan8686-coder <yaseenzaheer860@gmail.com>
2026-06-05 11:18:29 +0000

    datatype conversion

--- 01_basics/script.js
+++ 01_basics/script.js
@@ -1,1 +1,9 @@
-console.log("Yasin Khan")
+let isLoggedin = 1;
+
+// let booleanisLoggedin = Boolean(isLoggedin);
+// console.log(typeof (booleanisLoggedin));
+// console.log(typeof booleanisLoggedin);
+
+let booleanisLoggedin = String(isLoggedin);
+console.log(typeof (booleanisLoggedin));
+console.log(booleanisLoggedin);
