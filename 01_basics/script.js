f17b8d8ed0e4e40441368959a64bd2749e6afddd
Yasinkhan824 <myasinkhan824@gmail.com>
2026-06-06 09:41:43 +0000

    javascript

--- 01_basics/script.js
+++ 01_basics/script.js
@@ -7,3 +7,6 @@ let isLoggedin = 1;
 let booleanisLoggedin = String(isLoggedin);
 console.log(typeof (booleanisLoggedin));
 console.log(booleanisLoggedin);
+console.log("2" > 1);
+console.log(2 >= 1);
+console.log(2 != 1);
--- 01_basics/var.js
+++ 01_basics/var.js
@@ -1,8 +1,7 @@
-
 let accountId = 512799;
 let accountEmail = "Yasin824@google.com";
 var accountPassword = 12345;
 console.log(accountId,accountEmail,accountPassword);
 accountCity = "mardan";
 console.table([accountId, accountEmail, accountPassword, accountCity]);
-console.log(typeof "");
+console.log(typeof "hi hi hi");
